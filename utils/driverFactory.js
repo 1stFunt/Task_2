31c717f57aa874aac834c141bd80245a0ad77ae6
Funt <funt_88@mail.ru>
2025-08-06 18:48:20 +0300

    Test task completed

--- utils/driverFactory.js
+++ utils/driverFactory.js
@@ -2,24 +2,22 @@ const { Builder } = require('selenium-webdriver');
 const firefox = require('selenium-webdriver/firefox');
 const config = require('../config');
 
-/**
- * Фабрика для создания WebDriver на основе настроек из конфига
- * @returns {import('selenium-webdriver').WebDriver}
- */
 function createDriver() {
     if (process.env.GITHUB_ACTIONS) {
-        // В GitHub Actions явно указываем путь к Firefox
+        // В GitHub Actions явно указываем путь к бинарнику Firefox
         let options = new firefox.Options();
-        options.setBinary('/usr/bin/firefox'); // путь для Ubuntu runner в GH Actions
+        options.setBinary('/usr/bin/firefox'); // Проверить, что этот путь корректен на runner-е
 
         return new Builder()
-            .forBrowser(config.browser)
+            .forBrowser('firefox')
             .setFirefoxOptions(options)
             .build();
-    } else {
-        // Локально — дефолт
-        return new Builder().forBrowser(config.browser).build();
     }
+
+    // Локально просто берем из конфига
+    return new Builder()
+        .forBrowser(config.browser)
+        .build();
 }
 
 module.exports = { createDriver };
